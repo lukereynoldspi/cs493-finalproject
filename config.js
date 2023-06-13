2022ada9847b77b047d45e7bb62fbb836304cc37
lucasrouchy <lucasrouchy1@gmail.com>
2023-06-13 13:56:24 -0700

    reset commits to delete secuirty flaws.

---
+++ config.js
@@ -0,0 +1,13 @@
+const crypto = require('crypto');
+
+const generateRandomString = (length) => {
+  return crypto.randomBytes(length).toString('hex');
+};
+
+const jwtSecret = generateRandomString(32); 
+
+module.exports = {
+    JWT_SECRET: jwtSecret,
+    JWT_EXPIRATION_TIME: '24h'
+};
+  
