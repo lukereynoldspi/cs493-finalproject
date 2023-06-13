bc5ea5dcd6bae50ccb2fdb89aac96ef36987e9b6
lucasrouchy <lucasrouchy1@gmail.com>
2023-06-13 16:10:18 -0700

    first draft for assigment api endpoint. untested

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const router = module.exports = require('express').Router()
 
 router.use('/courses', require('./courses').router)
-// router.use('/assignments', require('./assignments').router)
+router.use('/assignments', require('./assignments').router)
 router.use('/users', require('./users').router)
 
 
--- models/submission.js
+++ models/submission.js
@@ -3,7 +3,7 @@ const submissionSchema = new mongoose.Schema({
   assignmentId: { type: Number, required: true },
   studentId: { type: Number, required: true },
   grade: { type: String, required: true },
-  timestamp: { type: String, required: true },
+  timestamp: { type: Date, required: true },
   file: { type: String, required: true }
 });
 const Submission = mongoose.model('Submission', submissionSchema);
