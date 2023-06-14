d9eaeffb215dbcb54e017a8f5c5cd831663fcc14
lukereynoldspi <lukereynoldspi@gmail.com>
2023-06-13 17:47:39 -0700

    finished creating dummy data

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  userid: { type: Number, required: true },
+  userId: { type: Number, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   role:{type: String, required: true}
