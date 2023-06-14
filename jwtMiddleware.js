d46a396708f834019e99989e1933eec430a2c526
lucasrouchy <lucasrouchy1@gmail.com>
2023-06-14 16:33:33 -0700

    JWT fixes

--- jwtMiddleware.js
+++ jwtMiddleware.js
@@ -5,16 +5,17 @@ const jwtMiddleware = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
   
     if (!token) {
-      return res.status(401).json({ message: 'Unauthorized' });
+      return res.status(401).json({ message: 'Unauthorized: No token' });
     }
   
     try {
-      const decoded = jwt.verify(token, JWT_SECRET);
-      req.user = decoded;
-      if (req.params.userId && req.params.userId !== req.user.id && !req.user.admin) {
-        return res.status(403).json({ message: 'Forbidden' });
-      }
-      next();
+      jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(403).json({ message: 'Forbidden: Invalid Token' });
+        }
+        req.user = decoded;
+        next();
+      });
     } catch (err) {
       res.status(401).json({ message: 'Invalid token' });
     }
--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   userId: { type: Number, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role:{type: String, required: true}
+  role: { type: String, required: true}
 });
 const User = mongoose.model('User', userSchema)
 module.exports = User
